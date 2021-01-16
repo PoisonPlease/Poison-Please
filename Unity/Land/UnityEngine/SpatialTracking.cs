373ebcdeb8c805fdc281e1c9f01ac115584c1106
AumKanokruk <ryw37248@rayongwit.ac.th>
2021-01-17 00:47:32 +0700

    Unity

---
+++ Unity/Land/UnityEngine/SpatialTracking.cs
@@ -0,0 +1,17 @@
+﻿/******************************************************************************
+ * Copyright (C) Ultraleap, Inc. 2011-2020.                                   *
+ *                                                                            *
+ * Use subject to the terms of the Apache License 2.0 available at            *
+ * http://www.apache.org/licenses/LICENSE-2.0, or another agreement           *
+ * between Ultraleap and you, your company or other organization.             *
+ ******************************************************************************/
+
+namespace UnityEngine
+{
+    internal class SpatialTracking
+    {
+        internal class TrackedPoseDriver
+        {
+        }
+    }
+}
