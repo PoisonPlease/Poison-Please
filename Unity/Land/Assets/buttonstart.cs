0466bb1abd25337494287a69e3176e019a4d891c
AumKanokruk <ryw37248@rayongwit.ac.th>
2021-01-17 00:51:01 +0700

    Unity

---
+++ Unity/Land/Assets/buttonstart.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class startbutton : MonoBehaviour
+{
+
+    public void onClickNextScene()
+    {
+        SceneManager.LoadScene("Ani_select");
+    }
+
+}
---
+++ Unity/Land/Assets/scenemanager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scenemanager : MonoBehaviour
+{
+    public void onClickNextScene()
+    {
+        SceneManager.LoadScene("Manumuna");
+    }
+}
